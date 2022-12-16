968b235dfa5ce9aa93c260b732f40dc64ec4ca27
jaysonadam <jaysonadam@gmail.com>
2022-12-17 01:00:11 +0700

    Underline below active nav link update

--- src/Components/Navbar/Navbar.js
+++ src/Components/Navbar/Navbar.js
@@ -2,22 +2,27 @@ import React from 'react';
 import { useLocation } from "react-router-dom";
 
 import './Navbar.css';
-import { Nav, Navbar } from "react-bootstrap";
+import { Nav, Navbar, Container } from "react-bootstrap";
 
 function Navigation() {
     const location = useLocation();
 
     return (
         <div>
-            <Navbar bg="dark" variant="dark">
-                <Nav activeKey={location.pathname}>
-                    <Nav.Link href='/'>
-                        Home
-                    </Nav.Link>
-                    <Nav.Link href='/products'>
-                        Products
-                    </Nav.Link>
-                </Nav>
+            <Navbar expand="md" bg="dark" variant="dark" >
+                <Container style={{ marginInline: '0px' }}>
+                    <Navbar.Toggle aria-controls="basic-navbar-nav" />
+                    <Navbar.Collapse id="basic-navbar-nav">
+                        <Nav activeKey={location.pathname}>
+                            <Nav.Link href='/'>
+                                Home
+                            </Nav.Link>
+                            <Nav.Link href='/products'>
+                                Products
+                            </Nav.Link>
+                        </Nav>
+                    </Navbar.Collapse>
+                </Container>
             </Navbar>
         </div>
     )
