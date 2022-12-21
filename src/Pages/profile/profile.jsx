f1bce4f5ee6f23b2bcfbc7daf663dc2ed584352c
jaysonadam <jaysonadam@gmail.com>
2022-12-21 14:36:40 +0700

    Start racing profile page

--- src/Pages/profile/profile.jsx
+++ src/Pages/profile/profile.jsx
@@ -1,8 +1,42 @@
 import React from 'react'
 
+import './profile.css';
+import { Carousel } from 'react-bootstrap';
+
+const slideImages = [
+  {
+    src: require('../../Images-Videos/images/motogp/ktm-motogp.jpg')
+  },
+  {
+    src: require('../../Images-Videos/images/motogp/motogp-1.jpg')
+  },
+  {
+    src: require('../../Images-Videos/images/motogp/motogp-2.jpg')
+  },
+  {
+    src: require('../../Images-Videos/images/motogp/motogp-3.jpg')
+  },
+  {
+    src: require('../../Images-Videos/images/motogp/motogp-4.jpg')
+  }
+];
+
 function Profile() {
+
   return (
-    <div>Racing Profile</div>
+    <div>
+      <h1 className='profile-head'>Ready to Race</h1>
+      <h2 className='profile-subhead'>We are motogp</h2>
+      <Carousel touch='true' interval={4000}>
+        {slideImages.map((slide) => {
+
+          return <Carousel.Item id="item">
+            <img src={slide.src} className="ima" />
+          </Carousel.Item>
+
+          })}
+      </Carousel>
+    </div>
   )
 }
 
