b10d79bf99bd017630c7d53d58ec6f6f5b4b25e4
jaysonadam <jaysonadam@gmail.com>
2022-12-21 16:41:51 +0700

    Racing profile done

--- src/Pages/profile/profile.jsx
+++ src/Pages/profile/profile.jsx
@@ -1,7 +1,7 @@
 import React from 'react'
 
 import './profile.css';
-import { Carousel } from 'react-bootstrap';
+import { Carousel, Card, Button } from 'react-bootstrap';
 import MOTOGP from '../../Images-Videos/videos/MOTOGP.mp4';
 
 const slideImages = [
@@ -22,10 +22,72 @@ const slideImages = [
   }
 ];
 
+const riders = [
+  {
+    name: 'Brad Binder',
+    class: 'MotoGP',
+    number: 33,
+    country: 'South Africa',
+    birthday: 'August 11th, 1995',
+    src: require('../../Images-Videos/images/motogp/riders/brad.jpg')
+  },
+  {
+    name: 'Miguel Oliveira',
+    class: 'MotoGP',
+    number: 88,
+    country: 'Portugal',
+    birthday: 'January 1st, 1995',
+    src: require('../../Images-Videos/images/motogp/riders/miguel.jpg')
+  },
+  {
+    name: 'Raul fernandez',
+    class: 'MotoGP',
+    number: 25,
+    country: 'Spain',
+    birthday: 'October 23rd, 2000',
+    src: require('../../Images-Videos/images/motogp/riders/raul.jpg')
+  },
+  {
+    name: 'Remy Gardner',
+    class: 'MotoGP',
+    number: 87,
+    country: 'Australia',
+    birthday: 'February 24th, 1998',
+    src: require('../../Images-Videos/images/motogp/riders/remy.jpg')
+  },
+  {
+    name: 'Jaume Masia',
+    class: 'Moto3',
+    number: 5,
+    country: 'Spain',
+    birthday: 'October 31st, 2000',
+    src: require('../../Images-Videos/images/motogp/riders/masia.jpg')
+  },
+  {
+    name: 'Pedro Acosta',
+    class: 'Moto2',
+    number: 51,
+    country: 'Spain',
+    birthday: 'May 25th, 2005',
+    src: require('../../Images-Videos/images/motogp/riders/pedro.jpg')
+  },
+  {
+    name: 'Deniz Oncu',
+    class: 'Moto3',
+    number: 53,
+    country: 'Turkey',
+    birthday: 'July 26th, 2003',
+    src: require('../../Images-Videos/images/motogp/riders/deniz.jpg')
+  },
+]
+
 function Profile() {
 
   return (
     <div>
+      <div className='d-flex justify-content-center mt-5'>
+        <img src={require('../../Images-Videos/images/Factory Racing Logo.jpg')} className='factory'/>
+      </div>
       <h1 className='profile-head'>Ready to Race</h1>
       <h2 className='profile-subhead'>We are motogp</h2>
       <Carousel touch='true' interval={4000}>
@@ -43,6 +105,27 @@ function Profile() {
       <video width='100%' loop playsinline controls className='video-ktm'>
         <source src={MOTOGP} type="video/mp4"></source>
       </video>
+
+      <h1 className='profile-head'>Meet our Riders</h1>
+
+      <div className='rider'>
+          {riders.map((rider) => {
+
+            return <Card className='kartu'>
+              <Card.Img src={rider.src} className='img-card'/>
+              <Card.ImgOverlay>
+                <div className='rider-img-text'>
+                  <div className='d-flex flex-column'>
+                    <h1>
+                      {rider.name}
+                    </h1>
+                    <Button className='rider-details'>Rider Details</Button>
+                  </div>
+                </div>
+              </Card.ImgOverlay>
+            </Card>
+          })}
+      </div>
     </div>
   )
 }
