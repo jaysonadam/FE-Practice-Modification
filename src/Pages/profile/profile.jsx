98e846589d5e9f25810cab685dfa30789163e757
jaysonadam <jaysonadam@gmail.com>
2022-12-22 23:59:53 +0700

    Solved image from json-server

--- src/Pages/profile/profile.jsx
+++ src/Pages/profile/profile.jsx
@@ -24,65 +24,6 @@ const slideImages = [
   }
 ];
 
-const riders = [
-  {
-    name: 'Brad Binder',
-    class: 'MotoGP',
-    number: 33,
-    country: 'South Africa',
-    birthday: 'August 11th, 1995',
-    src: require('../../Images-Videos/images/motogp/riders/brad.jpg')
-  },
-  {
-    name: 'Miguel Oliveira',
-    class: 'MotoGP',
-    number: 88,
-    country: 'Portugal',
-    birthday: 'January 1st, 1995',
-    src: require('../../Images-Videos/images/motogp/riders/miguel.jpg')
-  },
-  {
-    name: 'Raul fernandez',
-    class: 'MotoGP',
-    number: 25,
-    country: 'Spain',
-    birthday: 'October 23rd, 2000',
-    src: require('../../Images-Videos/images/motogp/riders/raul.jpg')
-  },
-  {
-    name: 'Remy Gardner',
-    class: 'MotoGP',
-    number: 87,
-    country: 'Australia',
-    birthday: 'February 24th, 1998',
-    src: require('../../Images-Videos/images/motogp/riders/remy.jpg')
-  },
-  {
-    name: 'Jaume Masia',
-    class: 'Moto3',
-    number: 5,
-    country: 'Spain',
-    birthday: 'October 31st, 2000',
-    src: require('../../Images-Videos/images/motogp/riders/masia.jpg')
-  },
-  {
-    name: 'Pedro Acosta',
-    class: 'Moto2',
-    number: 51,
-    country: 'Spain',
-    birthday: 'May 25th, 2005',
-    src: require('../../Images-Videos/images/motogp/riders/pedro.jpg')
-  },
-  {
-    name: 'Deniz Oncu',
-    class: 'Moto3',
-    number: 53,
-    country: 'Turkey',
-    birthday: 'July 26th, 2003',
-    src: require('../../Images-Videos/images/motogp/riders/deniz.jpg')
-  }
-]
-
 function Profile() {
   const params = useParams();
 
@@ -92,8 +33,6 @@ function Profile() {
     axios
         .get("/riders", { params: { id: params.id } })
         .then((res) => {
-          console.log(res)
-          console.log(res.data)
           setRider(res.data)
         })
         .catch((err) => {
@@ -130,7 +69,7 @@ function Profile() {
           {rider.map((rdr) => {
 
             return <Card className='kartu'>
-              <Card.Img src={rdr.src} className='img-card'/>
+              <Card.Img src={`${rdr.src}`} className='img-card'/>
               <Card.ImgOverlay>
                 <div className='rider-img-text'>
                   <div className='d-flex flex-column'>
