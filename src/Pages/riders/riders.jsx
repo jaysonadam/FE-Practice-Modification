2399d4f2ddcbba2d28cae978b122175abdb8705e
jaysonadam <jaysonadam@gmail.com>
2022-12-26 22:57:21 +0700

    Update image sizing at mobile view

--- src/Pages/riders/riders.jsx
+++ src/Pages/riders/riders.jsx
@@ -1,5 +1,7 @@
-import React, { useState, useEffect } from 'react';
+import React from 'react';
 import { useParams } from 'react-router-dom';
+
+import './riders.css';
 
 const riders = [
     {
@@ -41,7 +43,7 @@ const riders = [
     {
       id: 5,
       name: "Jaume Masia",
-      class: "Moto3",
+      class: "Moto 3",
       number: 5,
       country: "Spain",
       birthday: "October 31st, 2000",
@@ -50,7 +52,7 @@ const riders = [
     {
       id: 6,
       name: "Pedro Acosta",
-      class: "Moto2",
+      class: "Moto 2",
       number: 51,
       country: "Spain",
       birthday: "May 25th, 2005",
@@ -59,7 +61,7 @@ const riders = [
     {
       id: 7,
       name: "Deniz Oncu",
-      class: "Moto3",
+      class: "Moto 3",
       number: 53,
       country: "Turkey",
       birthday: "July 26th, 2003",
@@ -68,90 +70,21 @@ const riders = [
 ]
 
 function Riders() {
-    
     const params = useParams();
     const id = params.id;
-
-    // const [ riders, setRiders ] = useState([
-    // {
-    //     id: 1,
-    //     name: "Brad Binder",
-    //     class: "MotoGP",
-    //     number: 33,
-    //     country: "South Africa",
-    //     birthday: "August 11th, 1995",
-    //     src: require("../../Images-Videos/images/riders/brad.jpg")
-    //   },
-    //   {
-    //     id: 2,
-    //     name: "Miguel Oliveira",
-    //     class: "MotoGP",
-    //     number: 88,
-    //     country: "Portugal",
-    //     birthday: "January 1st, 1995",
-    //     src: require("../../Images-Videos/images/riders/miguel.jpg")
-    //   },
-    //   {
-    //     id: 3,
-    //     name: "Raul fernandez",
-    //     class: "MotoGP",
-    //     number: 25,
-    //     country: "Spain",
-    //     birthday: "October 23rd, 2000",
-    //     src: require("../../Images-Videos/images/riders/raul.jpg")
-    //   },
-    //   {
-    //     id: 4,
-    //     name: "Remy Gardner",
-    //     class: "MotoGP",
-    //     number: 87,
-    //     country: "Australia",
-    //     birthday: "February 24th, 1998",
-    //     src: require("../../Images-Videos/images/riders/remy.jpg")
-    //   },
-    //   {
-    //     id: 5,
-    //     name: "Jaume Masia",
-    //     class: "Moto3",
-    //     number: 5,
-    //     country: "Spain",
-    //     birthday: "October 31st, 2000",
-    //     src: require("../../Images-Videos/images/riders/masia.jpg")
-    //   },
-    //   {
-    //     id: 6,
-    //     name: "Pedro Acosta",
-    //     class: "Moto2",
-    //     number: 51,
-    //     country: "Spain",
-    //     birthday: "May 25th, 2005",
-    //     src: require("../../Images-Videos/images/riders/pedro.jpg")
-    //   },
-    //   {
-    //     id: 7,
-    //     name: "Deniz Oncu",
-    //     class: "Moto3",
-    //     number: 53,
-    //     country: "Turkey",
-    //     birthday: "July 26th, 2003",
-    //     src: require("../../Images-Videos/images/riders/deniz.jpg")
-    //   }]);
-
-    // useEffect(() => {
-    //     getDataById();
-    // }, []);
-
-    // const getDataById = () => {
-    //     setDetail(riders.filter(rider => rider.id === id ))
-    // };
 
     return (
         <div>
             {riders.filter(rider => rider.id == id).map(filtered => (
-                <div>
-                    <h1>{filtered.name}</h1>
-                    <h3>Class: {filtered.class}</h3>
-                    <img src={filtered.src}/>
+                <div className='d-flex flex-row flex-wrap'>
+                    <img src={filtered.src} className='rider-img'/>
+                    <div className='d-flex flex-column p-5'>
+                      <h1 className='rider-name-p'>{filtered.name}</h1>
+                      <h3 className='rider-info'>Class: {filtered.class}</h3>
+                      <h3 className='rider-info'>Number: {filtered.number}</h3>
+                      <h3 className='rider-info'>Nationality: {filtered.country}</h3>
+                      <h3 className='rider-info'>Birth date: {filtered.birthday}</h3>
+                    </div>
                 </div>
                 )
             
