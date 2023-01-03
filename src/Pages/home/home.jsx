14923133ab2d0ba7ae0e814aba71d580e6ff48fb
jaysonadam <jaysonadam@gmail.com>
2023-01-03 16:37:38 +0700

    Edit home page

--- src/Pages/home/home.jsx
+++ src/Pages/home/home.jsx
@@ -14,7 +14,7 @@ function Home() {
           <div className='cont'>
             <img src={require('../../Images-Videos/images/rc/rc.jpg')} className='bg-home' />
             <div className='w'>
-              <h1>Welcome to KTM</h1>
+              <h1 className='welcome'>Welcome to KTM</h1>
               <h3 className='sub'>Ready to Race</h3>
               <Button className='button-home' href="/products">
                 Our Bikes
